15c5ac563b73e59ddd30b69584edc21855501917
Swift <swiftx37@gmail.com>
2023-09-06 08:47:34 +0200

    Add project files.

---
+++ Notes.Domain/Note.cs
@@ -0,0 +1,17 @@
+﻿namespace Notes.Domain
+{
+    public class Note
+    {
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Details { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime? EditDate { get; set; }
+    }
+}
