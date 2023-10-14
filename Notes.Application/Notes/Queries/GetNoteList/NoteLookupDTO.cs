af163378e80e35ecab9e5c1fce7d5135b2386a8f
Swift <swiftx37@gmail.com>
2023-10-14 17:12:12 +0200

    Updating the NoteLookupDTO entity

--- Notes.Application/Notes/Queries/GetNoteList/NoteLookupDTO.cs
+++ Notes.Application/Notes/Queries/GetNoteList/NoteLookupDTO.cs
@@ -10,13 +10,21 @@ namespace Notes.Application.Notes.Queries.GetNoteList
 
         public string? Title { get; set; }
 
+        public string? Details { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
         public void Mapping(Profile profile)
         {
             profile.CreateMap<Note, NoteLookupDTO>()
                 .ForMember(noteDTO => noteDTO.Id,
                     opt => opt.MapFrom(note => note.Id))
                 .ForMember(noteDTO => noteDTO.Title,
-                    opt => opt.MapFrom(note => note.Title));
+                    opt => opt.MapFrom(note => note.Title))
+                .ForMember(noteDTO => noteDTO.Details,
+                    opt => opt.MapFrom(note => note.Details))
+                .ForMember(noteDTO => noteDTO.CreationDate,
+                    opt => opt.MapFrom(note => note.CreationDate));
         }
     }
 }
